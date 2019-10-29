bfb3948865e0cf1598c38fe86ec2886d9850e3ac
Colafornia <wangyilinchn@foxmail.com>
2019-10-29 17:32:16 +0800

    feat(urlshort): Init package urlshort

---
+++ urlshort/handler.go
@@ -0,0 +1,38 @@
+package urlshort
+
+import (
+	"net/http"
+	"gopkg.in/yaml.v2"
+)
+
+type PathURL struct {
+	Path string `yaml:"path"`
+	URL string `yaml:"url"`
+}
+
+func MapHandler(pathsToUrls map[string]string, fallback http.Handler) http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+		path := r.URL.Path
+		dest, ok := pathsToUrls[path]
+		if ok {
+			http.Redirect(w, r, dest, http.StatusFound)
+		}
+		fallback.ServeHTTP(w, r)
+	}
+}
+
+func YAMLHandler(yml []byte, fallback http.Handler) (http.HandlerFunc, error) {
+	var pathMap []PathURL
+	var pathsToUrls map[string]string
+
+	err := yaml.Unmarshal(yml, &pathMap)
+	if err != nil {
+		return nil, err
+	}
+
+	for _, v := range pathMap {
+		pathsToUrls[v.Path] = v.URL
+	}
+
+	return MapHandler(pathsToUrls, fallback), nil
+}
