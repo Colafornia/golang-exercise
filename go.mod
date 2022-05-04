c8ab6d88305fca8d1e65d8ef8dc1d4fb85d37595
Colafornia <me@colafornia.me>
2022-05-05 00:13:44 +0800

    feat: SSE crawler

--- go.mod
+++ go.mod
@@ -3,7 +3,7 @@ module github.com/Colafornia/golang-exercise
 go 1.12
 
 require (
-	github.com/PuerkitoBio/goquery v1.5.0
+	github.com/PuerkitoBio/goquery v1.5.0 // indirect
 	github.com/antchfx/htmlquery v1.1.0 // indirect
 	github.com/antchfx/xmlquery v1.1.0 // indirect
 	github.com/antchfx/xpath v1.1.0 // indirect
@@ -12,15 +12,8 @@ require (
 	github.com/kennygrant/sanitize v1.2.4 // indirect
 	github.com/saintfish/chardet v0.0.0-20120816061221-3af4cd4741ca // indirect
 	github.com/temoto/robotstxt v1.1.1 // indirect
-	golang.org/x/crypto v0.0.0-20191029031824-8986dd9e96cf // indirect
 	golang.org/x/net v0.0.0-20191105084925-a882066a44e0
-	golang.org/x/sync v0.0.0-20190911185100-cd5d95a43a6e // indirect
-	golang.org/x/sys v0.0.0-20191029155521-f43be2a4598c // indirect
-	golang.org/x/tools v0.0.0-20191031220737-6d8f1af9ccc0 // indirect
-	golang.org/x/xerrors v0.0.0-20191011141410-1b5146add898 // indirect
 	google.golang.org/appengine v1.6.5 // indirect
-	gopkg.in/gomail.v2 v2.0.0-20160411212932-81ebce5c23df // indirect
-	gopkg.in/resty.v1 v1.12.0
 	gopkg.in/yaml.v2 v2.2.4
 )
 
---
+++ sse-crawler/main.go
@@ -0,0 +1,129 @@
+package main
+
+import (
+	"bytes"
+	"encoding/json"
+	"fmt"
+	"github.com/PuerkitoBio/goquery"
+	"github.com/gocolly/colly"
+	"html/template"
+	"io/ioutil"
+	"net/http"
+	"os"
+	"time"
+)
+
+const ssePrefix = "http://www.sse.com.cn/"
+const (
+	ssePageLink = "http://www.sse.com.cn/home/webupdate/"
+)
+
+type article struct {
+	Title string
+	Time  string
+	Link  string
+}
+
+type updatedArticles struct {
+	Name     string
+	Time     string
+	Articles []article
+}
+
+var tpl *template.Template
+
+func init() {
+	time.LoadLocation("Asia/Shanghai")
+	t := time.Now()
+	fmt.Println("Location:", t.Location(), ":Time:", t)
+	tpl = template.Must(template.ParseFiles("./sse-crawler/template.gohtml"))
+}
+
+func sendRequest(info updatedArticles) {
+	type RequestBody struct {
+		Summary     string   `json:"summary"`
+		ContentType int      `json:"contentType"`
+		Content     string   `json:"content"`
+		AppToken    string   `json:"appToken"`
+		TopicIds    []string `json:"topicIds"`
+	}
+	var tplBytes bytes.Buffer
+	tpl.Execute(&tplBytes, info)
+	html := tplBytes.String()
+	fmt.Println(html)
+	if html == "" {
+		return
+	}
+	contentBody := RequestBody{
+		ContentType: 2,
+		Content:     html,
+		Summary:     "有更新：" + info.Articles[0].Title + "...",
+		AppToken:    os.Getenv("APP_TOKEN"),
+		TopicIds:    []string{"5747"},
+		// 测试频道    5749
+		// 正式频道    5747
+	}
+	content, _ := json.Marshal(contentBody)
+	resp, err := http.Post("http://wxpusher.zjiecode.com/api/send/message", "application/json", bytes.NewBuffer(content))
+	if err != nil {
+		fmt.Println("err:", err)
+	}
+	defer resp.Body.Close()
+	body, _ := ioutil.ReadAll(resp.Body)
+	fmt.Println(string(body))
+}
+
+func crawlrateInfo() updatedArticles {
+	c := colly.NewCollector()
+	var info updatedArticles
+	yesterday := time.Now().Add(-24 * 7 * time.Hour)
+	fmt.Println(yesterday)
+	c.OnHTML("body", func(e *colly.HTMLElement) {
+		var articles = make([]article, 0, 10)
+		e.DOM.Find("#sse_list_1 dd").Each(func(i int, selection *goquery.Selection) {
+			selection.Find("span").Text()
+			link, ok := selection.Find("a").Attr("href")
+			var _article article
+			updateDate, err := time.Parse("2006-01-02", selection.Find("span").Text())
+			if err != nil {
+				return
+			}
+			isAfterTimeLimit := updateDate.After(yesterday)
+			if ok && isAfterTimeLimit {
+				_article = article{
+					Title: selection.Find("a").Text(),
+					Time:  selection.Find("span").Text(),
+					Link:  ssePrefix + link,
+				}
+				articles = append(articles, _article)
+				fmt.Println(selection.Find("span").Text())
+				fmt.Println(selection.Find("a").Text())
+				fmt.Println(ssePrefix + link)
+			}
+		})
+		info = updatedArticles{
+			Name:     "上海证券交易所",
+			Time:     time.Now().Format("2006-01-02"),
+			Articles: articles,
+		}
+		fmt.Println(info)
+	})
+
+	err := c.Visit(ssePageLink)
+
+	if err != nil {
+		fmt.Println("Visit SSEPageLink Error")
+	}
+	return info
+}
+
+func main() {
+	fmt.Println(os.Args)
+	info := crawlrateInfo()
+	fmt.Println(info)
+	if len(info.Articles) > 0 {
+		sendRequest(info)
+	} else {
+		fmt.Println("No update, No notify")
+	}
+}
