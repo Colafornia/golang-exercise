6021b621008465cfb33af0cc0d702ce4c6e38b7d
Colafornia <me@colafornia.me>
2022-05-12 01:28:17 +0800

    feat: Add CSRC origin

--- go.mod
+++ go.mod
@@ -12,6 +12,7 @@ require (
 	github.com/kennygrant/sanitize v1.2.4 // indirect
 	github.com/saintfish/chardet v0.0.0-20120816061221-3af4cd4741ca // indirect
 	github.com/temoto/robotstxt v1.1.1 // indirect
+	github.com/tidwall/gjson v1.14.1 // indirect
 	golang.org/x/net v0.0.0-20191105084925-a882066a44e0
 	google.golang.org/appengine v1.6.5 // indirect
 	gopkg.in/yaml.v2 v2.2.4
--- sse-crawler/main.go
+++ sse-crawler/main.go
@@ -6,16 +6,21 @@ import (
 	"fmt"
 	"github.com/PuerkitoBio/goquery"
 	"github.com/gocolly/colly"
+	"github.com/tidwall/gjson"
 	"html/template"
 	"io/ioutil"
 	"net/http"
 	"os"
+	"sync"
 	"time"
 )
 
-const ssePrefix = "http://www.sse.com.cn/"
+const ssePrefix = "http://www.sse.com.cn"
+const nerisPrefix = "https://neris.csrc.gov.cn/falvfagui/rdqsHeader/mainbody?navbarId=1&secFutrsLawId="
 const (
-	ssePageLink = "http://www.sse.com.cn/home/webupdate/"
+	ssePageLink   = "http://www.sse.com.cn/home/webupdate/"
+	nerisPageLink = "https://neris.csrc.gov.cn/falvfagui/"
+	nerisUrl      = "https://neris.csrc.gov.cn/falvfagui/rdqsHeader/informationController"
 )
 
 type article struct {
@@ -25,12 +30,15 @@ type article struct {
 }
 
 type updatedArticles struct {
+	Origin   string
 	Name     string
 	Time     string
 	Articles []article
 }
 
 var tpl *template.Template
+
+var yesterday = time.Now().Add(-36 * time.Hour)
 
 func init() {
 	time.LoadLocation("Asia/Shanghai")
@@ -39,7 +47,7 @@ func init() {
 	tpl = template.Must(template.ParseFiles("./sse-crawler/template.gohtml"))
 }
 
-func sendRequest(info updatedArticles) {
+func sendRequest(info []updatedArticles) {
 	type RequestBody struct {
 		Summary     string   `json:"summary"`
 		ContentType int      `json:"contentType"`
@@ -57,7 +65,7 @@ func sendRequest(info updatedArticles) {
 	contentBody := RequestBody{
 		ContentType: 2,
 		Content:     html,
-		Summary:     "有更新：" + info.Articles[0].Title + "...",
+		Summary:     "有更新：" + info[0].Articles[0].Title + "...",
 		AppToken:    os.Getenv("APP_TOKEN"),
 		TopicIds:    []string{"5747"},
 		// 测试频道    5749
@@ -76,7 +84,6 @@ func sendRequest(info updatedArticles) {
 func crawlrateInfo() updatedArticles {
 	c := colly.NewCollector()
 	var info updatedArticles
-	yesterday := time.Now().Add(-36 * time.Hour)
 	fmt.Println(yesterday)
 	c.OnHTML("body", func(e *colly.HTMLElement) {
 		var articles = make([]article, 0, 10)
@@ -102,6 +109,7 @@ func crawlrateInfo() updatedArticles {
 			}
 		})
 		info = updatedArticles{
+			Origin:   ssePageLink,
 			Name:     "上海证券交易所",
 			Time:     time.Now().Format("2006-01-02"),
 			Articles: articles,
@@ -117,11 +125,66 @@ func crawlrateInfo() updatedArticles {
 	return info
 }
 
+func requestNerisInfo() updatedArticles {
+	resp, err := http.Get(nerisUrl)
+	if err != nil {
+		fmt.Println("Request Neris API Error")
+		fmt.Println(err)
+	}
+	body, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		fmt.Println(err)
+	}
+	var articles = make([]article, 0, 10)
+	gjson.Get(string(body), "pageUtil.pageList").ForEach(func(key, value gjson.Result) bool {
+		updateDate, _ := time.Parse("20060102", value.Get("secFutrsLawVersion").String())
+		isAfterTimeLimit := updateDate.After(yesterday)
+		if isAfterTimeLimit {
+			link := nerisPrefix + value.Get("secFutrsLawId").String()
+			_article := article{
+				Title: value.Get("secFutrsLawName").String(),
+				Time:  updateDate.Format("2006-01-02"),
+				Link:  link,
+			}
+			articles = append(articles, _article)
+			return true
+		}
+		return false
+	})
+	fmt.Println(articles)
+	info := updatedArticles{
+		Origin:   nerisPageLink,
+		Name:     "证监会",
+		Time:     time.Now().Format("2006-01-02"),
+		Articles: articles,
+	}
+	return info
+}
+
 func main() {
 	fmt.Println(os.Args)
-	info := crawlrateInfo()
-	fmt.Println(info)
-	if len(info.Articles) > 0 {
+	var wg sync.WaitGroup
+	var info []updatedArticles
+	var nerisInfo updatedArticles
+	var sseInfo updatedArticles
+	wg.Add(2)
+	go func() {
+		nerisInfo = requestNerisInfo()
+		if len(nerisInfo.Articles) > 0 {
+			info = append(info, nerisInfo)
+		}
+		wg.Done()
+	}()
+	go func() {
+		sseInfo = crawlrateInfo()
+		if len(sseInfo.Articles) > 0 {
+			info = append(info, sseInfo)
+		}
+		wg.Done()
+	}()
+	wg.Wait()
+	fmt.Println(nerisInfo, sseInfo)
+	if len(nerisInfo.Articles)+len(sseInfo.Articles) > 0 {
 		sendRequest(info)
 	} else {
 		fmt.Println("No update, No notify")
